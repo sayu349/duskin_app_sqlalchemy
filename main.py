be47960549a702b0c62b60dc9240a55d0c19785d
sayu349 <sayu10370@gmail.com>
2023-11-26 15:54:00 +0900

    update

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 # 顧客情報入力フォームをインポート
-from forms import UserInfoForm
-# create_duskin_db.py から Customer モデルをインポート
-from create_duskin_db import Customer
+from forms import AddCustomerForm
+# db_info.py から Customer モデルをインポート
+from db_info import db
+from db_info import Customer
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///duskin.db'
-db = SQLAlchemy(app)
+db.init_app(app)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    form = UserInfoForm(request.form)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/customer")
+def customer():
+    # DB接続
+    with app.app_context():
+        # DBから必要な情報を取得
+        customer_list = Customer.query.all()
+    # データ出力
+    return render_template("customer.html", customer_list=customer_list)
+
+@app.route("/add_customer", methods=["GET", "POST"])
+def add_customer():
+    form = AddCustomerForm(request.form)
     # Post
     if request.method == "POST":
         # データ受け取り
         customer_name = form.customer_name.data
-        tel = form.tel.data
+        phone_number = form.phone_number.data
         # DBにデータ追加
-        new_customer = Customer(customer_name=customer_name, phone_number=tel)  # Customer インスタンスの作成
+        new_customer = Customer(customer_name=customer_name, phone_number=phone_number)  # Customer インスタンスの作成
         db.session.add(new_customer)
         db.session.commit()
-        return render_template("success.html")
+        db.session.close()
+        # データ出力
+        return render_template("success.html", customer_name=customer_name,phone_number=phone_number)
     # Get
     else:
-        return render_template("index.html", form=form)
+        return render_template("add_customer.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -5,9 +5,10 @@ flask-sqlalchemyを使って、dbを構築・操作します。
 db作成からデータ入力までのサンプルコードとして使えます。
 
 ## 概要
+- db_info.pyに全てのdbの構成情報を書き記しています
 - create_duskin_db.pyで、dbを作成
 - forms.pyにて、入力フォームフォーマットを作成
-- main.pyにて、データの受け渡し、dbへデータを入力
+- main.pyにて、データの受け渡し、dbへデータ入力・出力をします
 
 ## 使い方
 - dbを作成（再作成）する場合
@@ -17,5 +18,6 @@ db作成からデータ入力までのサンプルコードとして使えます
 
 ## 捕捉
 - create_duskin_db.pyは、dbを削除したときにだけ使います
+    - 削除したdbの再作成には、create_duskin_db.pyを使うということ
 - dbは、"instance/duskin.db"にあります
     - db内を覗きたい場合には、VSCodeの拡張機能である"SQLite Viewer"などを活用してください
